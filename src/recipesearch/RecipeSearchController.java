5bd96953bcf6598e60af9192cdd64d4d2d2cf10b
tarikpo <tarik.porobic@outlook.com>
2020-04-01 16:16:00 +0200

    Add updated refcielistitem

--- src/recipesearch/RecipeSearchController.java
+++ src/recipesearch/RecipeSearchController.java
@@ -5,12 +5,12 @@ import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 
+import com.sun.jdi.ObjectReference;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.Slider;
-import javafx.scene.control.Spinner;
+import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.FlowPane;
 import se.chalmers.ait.dat215.lab2.Recipe;
@@ -34,6 +34,8 @@ public class RecipeSearchController implements Initializable {
     @FXML
     private RadioButton allRB;
     @FXML
+    private RadioButton easyRB;
+    @FXML
     private RadioButton mediumRB;
     @FXML
     private RadioButton difficulRB;
@@ -54,6 +56,8 @@ public class RecipeSearchController implements Initializable {
         }
 
 
+
+
         System.out.println(recipes);
     }
 
@@ -61,6 +65,89 @@ public class RecipeSearchController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         updateRecipeList();
+
+        headingrediensCB.getItems().addAll("Visa alla", "Kött", "Fisk", "Kyckling", "Vegetarisk");
+        headingrediensCB.getSelectionModel().select("Visa alla");
+        headingrediensCB.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
+
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                bc.setMainIngredient(newValue);
+                updateRecipeList();
+            }
+        });
+
+        cuisineCB.getItems().addAll("Visa alla", "Sverige", "Grekland", "Indien", "Asien","Afrika","Frankrike");
+        cuisineCB.getSelectionModel().select("Visa alla");
+        cuisineCB.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
+
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                bc.setCuisine(newValue);
+                updateRecipeList();
+            }
+        });
+
+        ToggleGroup difficultyToggleGroup = new ToggleGroup();
+        allRB.setToggleGroup(difficultyToggleGroup);
+        easyRB.setToggleGroup(difficultyToggleGroup);
+        mediumRB.setToggleGroup(difficultyToggleGroup);
+        difficulRB.setToggleGroup(difficultyToggleGroup);
+        allRB.setSelected(true);
+
+        difficultyToggleGroup.selectedToggleProperty().addListener(new ChangeListener<Toggle>() {
+
+            @Override
+            public void changed(ObservableValue<? extends Toggle> observable, Toggle oldValue, Toggle newValue) {
+
+                if (difficultyToggleGroup.getSelectedToggle() != null) {
+                    RadioButton selected = (RadioButton) difficultyToggleGroup.getSelectedToggle();
+                    bc.setDifficulty(selected.getText());
+                    updateRecipeList();
+                }
+            }
+        });
+
+        SpinnerValueFactory<Integer> valueFactory = new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 999, 0, 10);
+        maxpriceSpin.setValueFactory(valueFactory);
+        maxpriceSpin.valueProperty().addListener(new ChangeListener<Integer>() {
+
+            @Override
+            public void changed(ObservableValue<? extends Integer> observable, Integer oldValue, Integer newValue) {
+                    bc.setMaxPrice(newValue);
+                    updateRecipeList();
+            }
+        });
+        maxpriceSpin.focusedProperty().addListener(new ChangeListener<Boolean>() {
+
+            @Override
+            public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
+
+                if(newValue){
+                    //focusgained - do nothing
+                }
+                else{
+                    Integer value = Integer.valueOf(maxpriceSpin.getEditor().getText());
+                    bc.setMaxPrice(value);
+                    updateRecipeList();
+                }
+
+            }
+        });
+
+        maxtimeSlide.valueProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+                if(newValue != null && !newValue.equals(oldValue) && !maxtimeSlide.isValueChanging()) {
+                    bc.setMaxTime(newValue);
+                    updateRecipeList();
+                }
+
+
+            }
+        });
+
+
     }
 
 
